bf757913c75b726554c9519bcee173cf635f8aaa
doreemii <160958185+doreemii@users.noreply.github.com>
2024-05-08 06:36:29 +0300

    Add files via upload

---
+++ test/java/com/FinalPharma/finalPharma/FinalPharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinalPharma.finalPharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalPharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
